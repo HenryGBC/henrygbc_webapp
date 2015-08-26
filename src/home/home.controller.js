bf95c0ad62ee72c1543c2abd61aa497bbb5f986e
henrygbc <henry.bravo89@gmail.com>
2015-08-25 23:27:28 -0430

    list post style

--- src/home/home.controller.js
+++ src/home/home.controller.js
@@ -7,7 +7,7 @@ HomeController.$inject = ['$scope', 'blogService'];
 function HomeController ($scope, blogService){
     var vm = this;
     init();
-
+    vm.posts = ['1', '2', '3', '4', '5'];
     function init(){
         //blogService.getPosts();
     }
